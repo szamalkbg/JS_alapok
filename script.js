83435814ca4183126066d967fa3b22c7200c1f5a
szamalkbg <solomyr@proton.me>
2024-02-29 12:16:43 +0100

    gyak1

--- script.js
+++ script.js
@@ -2,14 +2,30 @@ window.addEventListener("load", function () {
     elemekElerese1();
     elemekElerese2();
     elemekElerese3();
+    elemekElerese4();
 });
 
 function elemekElerese1() {
-    /**Ide jön az első feladat */
+    const ELEM = document.querySelectorAll("section h2")[0]
+    const cim = ELEM.innerHTML;
+    console.log(cim)
 }
-function elemekElerese1() {
-    /**Ide jön az 2.  feladat */
+
+function elemekElerese2() {
+    const ELEM2=document.querySelectorAll("#ide")[0]
+    ELEM2.innerHTML = "<p>Jó reggelt!</p>"
 }
-function elemekElerese1() {
-    /**Ide jön az 3. feladat */
+function elemekElerese3() {
+    const ELEM3=document.querySelectorAll(".ide")[0]
+    ELEM3.innerHTML = "<p>Jó reggelt!</p>"
+}
+function elemekElerese4() {
+    const ELEM4=document.querySelectorAll("lista")[0]
+    let txt = "<ul>"
+    let veletlenszam = Math.floor(Math.random() * (31 - 10 + 1)) + 10;
+    for (let i=0; i < 5; i++) {
+        txt += "<li>"+veletlenszam+"</li>";
+    }
+    txt='</ul>'
+    ELEM4.innerHTML=txt
 }
